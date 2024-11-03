06c6549614c4a9b6e55680985135c289f7e3f6b0
Musicode1 <MusiCode3@gmail.com>
2024-11-03 23:55:14 +0200

    הוספת סיומת לייבוא ./video-element.js

--- video-element-cover/load-external-script.js
+++ video-element-cover/load-external-script.js
@@ -1,9 +1,10 @@
 
-export function loadExternalScript(url) {
+function loadExternalScript(url) {
     const script = document.createElement('script'); 
     script.src = url; 
     script.async = true; 
+    script.type = 'module';
     document.head.appendChild(script); 
 }
 
-
+loadExternalScript('https://musicode1.github.io/FullyKiosk/video-element-cover/index.js?v=0.1')
